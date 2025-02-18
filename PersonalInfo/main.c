144dc9527bd470c7dcca2e575b6ef494287252a8
Dalia Qdeih <daliaqdeih895@gmail.com>
2025-02-18 14:54:40 -0500

    main

---
+++ PersonalInfo/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    //This exercise asks for the Personal Info in separate lines of code.
+    printf ("\n" "Lucinda Potter");
+    printf ("\n" "6/24/1992");
+    printf ("\n" "work 000-000-0101");
+    printf ("\n" "cell 000-000-0189");
+    return 0;
+}
