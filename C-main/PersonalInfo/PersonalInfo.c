ac264b2ef209873518444bd553bbcaeaef88de43
Qdeih <dqdeih@hawkmail.hccfl.edu>
2025-04-01 17:21:16 -0400

    Initial commit

---
+++ C-main/PersonalInfo/PersonalInfo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+    //This exercise asks for the Personal Info in separate lines of code.
+    printf ("\n" "Lucinda Potter");
+    printf ("\n" "6/24/1992");
+    printf ("\n" "work 000-000-0101");
+    printf ("\n" "cell 000-000-0189");
+
+    printf ("\n" "John Smith");
+    printf ("\n" "8/15/1978");
+    printf ("\n" "work 000-111-0101");
+    printf ("\n" "cell 000-111-0189");
+
+    printf ("\n" "Chris Johnson");
+    printf ("\n" "12/01/1987");
+    printf ("\n" "work 000-222-0101");
+    printf ("\n" "cell 000-222-0189");
+
+    printf ("\n" "Robert Hall");
+    printf ("\n" "2/27/1949");
+    printf ("\n" "work 000-444-0101");
+    printf ("\n" "cell 000-444-0189");
+    return 0;
+}
---
+++ C-main/Twitter/Twitter.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char message[140]; // This array creates the character limit for the tweet.
+    printf("What's happening? "); // Replicated Twitter's opening message.
+    fgets(message, sizeof(message), stdin); // Allows for input to be entered in the terminal.
+
+    // This calculates the length of the message
+    int messageLength = strlen(message);
+    
+    if (messageLength <= 140) { // If or else statement that calculates whether the input satisfies.
+        printf("Posted");
+    } else {
+        printf("Rejected"); // Second half of If or else statement for a positive output.
+    }
+    return 0;
+} 
